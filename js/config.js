f05f66f58c305711bf5567ee0315d05513f974de
Nishankumar <84121106+Nishankumar@users.noreply.github.com>
2025-04-17 12:40:02 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_cgaIHkLGz', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '22petdfv65138k1tntlo15s75k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientSecret: 'fsqhv84o865au0vrgp9l552doan2einhf2aljomhbiutch2hkef',
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
